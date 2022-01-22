1731deb59989c6e1e3d535c8e7ce8f4d81135958
Natanael Urena <NatanaelUF17@gmail.com>
2022-01-22 12:32:59 -0400

    ✅ Adding TodoItem  repository

---
+++ ToDo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnTodoAdded;
+        event EventHandler<TodoItem> OnTodoUpdated;
+        event EventHandler<TodoItem> OnTodoRemoved;
+
+        Task<List<TodoItem>> GetTodos();
+        Task AddTodo(TodoItem todo);
+        Task UpdateTodo(TodoItem todo);
+        Task RemoveTodo(TodoItem todo);
+        Task AddOrUpdate(TodoItem todo);
+    }
+}
---
+++ ToDo/Repositories/TodoItemRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using ToDo.Models;
+
+namespace ToDo.Repositories
+{
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        public event EventHandler<TodoItem> OnTodoAdded;
+        public event EventHandler<TodoItem> OnTodoUpdated;
+        public event EventHandler<TodoItem> OnTodoRemoved;
+
+        public SQLiteAsyncConnection dbConnection;
+
+        public async Task AddOrUpdate(TodoItem todo)
+        {
+            if (todo.Id.Equals(0))
+            {
+                await AddTodo(todo);
+            }
+            await UpdateTodo(todo);
+        }
+
+        public async Task<List<TodoItem>> GetTodos()
+        {
+            await CreateConnection();
+            return await dbConnection.Table<TodoItem>().ToListAsync();
+        }
+
+        public async Task AddTodo(TodoItem todo)
+        {
+            await CreateConnection();
+            await dbConnection.InsertAsync(todo);
+            OnTodoAdded?.Invoke(this, todo);
+        }
+
+        public async Task UpdateTodo(TodoItem todo)
+        {
+            await CreateConnection();
+            await dbConnection.UpdateAsync(todo);
+            OnTodoUpdated?.Invoke(this, todo);
+        }
+
+        public async Task RemoveTodo(TodoItem todo)
+        {
+            await CreateConnection();
+            await dbConnection.DeleteAsync(todo);
+            OnTodoRemoved?.Invoke(this, todo);
+        }
+
+        private async Task CreateConnection()
+        {
+            if (dbConnection != null)
+            {
+                return;
+            }
+
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var databasePath = Path.Combine(documentPath, "Todos.db");
+
+            dbConnection = new SQLiteAsyncConnection(databasePath);
+            await dbConnection.CreateTableAsync<TodoItem>();
+
+            if (await dbConnection.Table<TodoItem>().CountAsync() == 0)
+            {
+                await dbConnection.InsertAsync(new TodoItem
+                {
+                    Title = "Learning Xamarin Forms",
+                    IsCompleted = false,
+                    Due = DateTime.Now
+                });
+            }
+        }
+    }
+}
